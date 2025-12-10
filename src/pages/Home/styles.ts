99d12076d4c1312d2a8ec0f69de3042d7af8e494
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-12-10 20:51:33 -0300

    feat(home): add styled inputs and countdown button; improve form layout

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -51,10 +51,16 @@ const BaseInput = styled.input`
 
 export const TaskInput = styled(BaseInput)`
   flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
+
 `
 
 export const MinutesAmountInput = styled(BaseInput)`
   width: 4rem;
+  
 `
 
 export const CountdownContainer = styled.div`
