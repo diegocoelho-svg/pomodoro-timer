6b7b4d47a78ca72598a63f04dd929894a51e9cdb
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-12-10 20:37:22 -0300

    feat(home): add styled inputs and countdown button component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -26,6 +26,10 @@ export const HeaderContainer = styled.header`
         border-bottom: 3px solid ${props => props.theme["green-500"]}
       }
 
+      &:focus {
+        box-shadow: none;
+      }
+
       &.active {
         color: ${props => props.theme["green-500"]}
       }
